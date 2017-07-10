e482e62cd633bb5b6300f043b18d67e2bd72e21b
wrathematics <wrathematics@gmail.com>
2017-07-10 13:43:56 -0400

    add shorthand for converters

--- R/converters.r
+++ R/converters.r
@@ -8,13 +8,19 @@ mat2spm = function(x)
   new("spm", ptr=ptr)
 }
 
+#' @export
+fl = mat2spm
+
 
 
 #' @export
 spm2mat = function(x)
 {
   if (!is.spm(x))
-    stop("input 'x' must be a spm (single precision matrix)")
+    stop("input 'x' must be a spv/spm (single precision vector/matrix)")
   
   .Call(R_spm2mat, x@ptr)
 }
+
+#' @export
+dbl = spm2mat
