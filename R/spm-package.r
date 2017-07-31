5a3d23b2bd95d6d4155f04addc6493fc825d2668
wrathematics <wrathematics@gmail.com>
2017-07-31 09:27:49 -0400

    note utils import

--- R/spm-package.r
+++ R/spm-package.r
@@ -3,15 +3,17 @@
 #' @description
 #' TODO
 #' 
+#' @importFrom utils capture.output
 #' @import methods
 #' 
 #' @useDynLib spm R_mat2spm R_spm2mat R_crossprod_spm R_crossprod_spmspm
-#'     R_tcrossprod_spm R_tcrossprod_spmspm R_matmult_spm R_xpose_spm
-#'     R_diag_spm R_chol_spm R_solve_spm R_solve_spmspm R_bracket_spm
-#'     R_nrow_spm R_ncol_spm R_dim_spm R_isSymmetric_spm
+#'   R_tcrossprod_spm R_tcrossprod_spmspm R_matmult_spm R_xpose_spm
+#'   R_diag_spm R_diagmat_spm R_chol_spm R_solve_spm R_solve_spmspm
+#'   R_bracket_spm R_nrow_spm R_ncol_spm R_dim_spm R_isSymmetric_spm R_svd_spm
+#'   R_isavec_spm R_qr_spm R_qrQ_spm R_qrR_spm R_numbytes_spm
 #' 
 #' @docType package
-#' @name coop-package
+#' @name spm-package
 #' @author Drew Schmidt
 #' @keywords package
 NULL
