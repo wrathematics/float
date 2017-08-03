7b670f6a1a2d7175ee86e270d4497b7c7dc11276
wrathematics <wrathematics@gmail.com>
2017-08-03 09:20:40 -0400

    move xpose to header

--- src/xpose.c
+++ src/xpose.c
@@ -1,23 +1,5 @@
 #include "spm.h"
-
-
-static inline void xpose(const int m, const int n, const float *const restrict x, float *restrict tx)
-{
-  const int blocksize = 8; // TODO check cache line explicitly
-  
-  for (int j=0; j<n; j+=blocksize)
-  {
-    for (int i=0; i<m; i+=blocksize)
-    {
-      for (int col=j; col<j+blocksize && col<n; ++col)
-      {
-        for (int row=i; row<i+blocksize && row<m; ++row)
-          tx[col + n*row] = x[row + m*col];
-      }
-    }
-  }
-}
-
+#include "xpose.h"
 
 
 SEXP R_xpose_spm(SEXP x_ptr)
---
+++ src/xpose.h
@@ -0,0 +1,23 @@
+#ifndef SPM_XPOSE_H_
+#define SPM_XPOSE_H_
+
+
+static inline void xpose(const int m, const int n, const float *const restrict x, float *restrict tx)
+{
+  const int blocksize = 8; // TODO check cache line explicitly
+  
+  for (int j=0; j<n; j+=blocksize)
+  {
+    for (int i=0; i<m; i+=blocksize)
+    {
+      for (int col=j; col<j+blocksize && col<n; ++col)
+      {
+        for (int row=i; row<i+blocksize && row<m; ++row)
+          tx[col + n*row] = x[row + m*col];
+      }
+    }
+  }
+}
+
+
+#endif
