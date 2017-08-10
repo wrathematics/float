44770d02f9e7e01b31e0dbc4b42244ba5bb48f27
wrathematics <wrathematics@gmail.com>
2017-08-10 10:30:14 -0400

    add comparison tests

--- tests/comparison.r
+++ tests/comparison.r
@@ -1,7 +1,27 @@
-# suppressPackageStartupMessages(library(spm))
-# set.seed(1234)
-# tol = 1e-6
+suppressPackageStartupMessages(library(spm))
+set.seed(1234)
 
-# same = function(test, truth) stopifnot(all.equal(test, truth, tol=tol))
+same = function(test, truth) stopifnot(identical(test, truth))
+tester = function(s, x, test)
+{
+  same(s >  test, x > test)
+  same(s >= test, x >= test)
+  same(s == test, x == test)
+  same(s <  test, x < test)
+  same(s <= test, x <= test)
+  
+  same(test >  s, test > x)
+  same(test >= s, test >= x)
+  same(test == s, test == x)
+  same(test <  s, test < x)
+  same(test <= s, test <= x)
+}
 
-### TODO
+x = matrix(-4:5, 5)
+s = fl(x)
+
+tester(s, x, 0L)
+tester(s, x, 0.0)
+tester(s, x, fl(0))
+tester(s, x, s)
+tester(s, x, x)
