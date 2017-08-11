fae5297e666b2da53403e01813a17a3760cef72f
wrathematics <wrathematics@gmail.com>
2017-08-11 08:18:26 -0400

    add a strictness rule to converters

--- R/converters.r
+++ R/converters.r
@@ -9,6 +9,9 @@
 #' 
 #' @param x
 #' A numeric or float vector/matrix.
+#' @param strict
+#' Should the function error if given the wrong kind of input? Otherwise it just
+#' silently returns the input.
 #' 
 #' @return
 #' The data stored in the type of whatever was asked for (the opposite of the
@@ -33,10 +36,15 @@ NULL
 
 #' @rdname converters
 #' @export
-mat2spm = function(x)
+mat2spm = function(x, strict=FALSE)
 {
   if (!is.numeric(x) && !is.logical(x))
-    stop("input 'x' must be a numeric or logical vector/matrix")
+  {
+    if (is.spm && isTRUE(strict))
+      return(x)
+    else
+      stop("input 'x' must be a numeric or logical vector/matrix")
+  }
   
   ptr = .Call(R_mat2spm, x)
   new("spm", ptr=ptr)
@@ -50,10 +58,15 @@ fl = mat2spm
 
 #' @rdname converters
 #' @export
-spm2mat = function(x)
+spm2mat = function(x, strict=FALSE)
 {
   if (!is.spm(x))
-    stop("input 'x' must be a spv/spm (single precision vector/matrix)")
+  {
+    if (isTRUE(strict))
+      stop("input 'x' must be a spv/spm (single precision vector/matrix)")
+    else
+      return(x)
+  }
   
   .Call(R_spm2mat, x@ptr)
 }
