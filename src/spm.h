38a52af809906763e583efa8029643d6a41f4ba1
wrathematics <wrathematics@gmail.com>
2017-08-11 13:49:58 -0400

    error when getting a null ptr

--- src/spm.h
+++ src/spm.h
@@ -12,8 +12,16 @@ extern float NA_FLOAT;
 
 #define CHARPT(x,i) ((char*)CHAR(STRING_ELT(x,i)))
 
+static inline matrix_t* getRptr_safe(void *ptr)
+{
+  if (ptr == NULL)
+    error("invalid spm object; pointer is NULL");
+  
+  return (matrix_t*)R_ExternalPtrAddr(ptr);
+}
+
 #define newRptr(ptr,Rptr,fin) PROTECT(Rptr = R_MakeExternalPtr(ptr, R_NilValue, R_NilValue));R_RegisterCFinalizerEx(Rptr, fin, TRUE)
-#define getRptr(ptr) R_ExternalPtrAddr(ptr);
+#define getRptr(ptr) getRptr_safe(ptr)
 
 static inline void matfin(SEXP ptr)
 {
