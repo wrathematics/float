8de9cc40d030577890aea4800f613394085732f7
wrathematics <wrathematics@gmail.com>
2017-10-19 10:36:39 -0400

    clarify args in eigen test

--- tests/eigen.r
+++ tests/eigen.r
@@ -10,12 +10,12 @@ s = flrnorm(m, n)
 cp_s = crossprod(s)
 cp_d = dbl(cp_s)
 
-test = eigen(cp_s, TRUE, TRUE)
-truth = eigen(cp_d, TRUE, TRUE)
+test = eigen(cp_s, symmetric=TRUE, only.values=TRUE)
+truth = eigen(cp_d, symmetric=TRUE, only.values=TRUE)
 stopifnot(all.equal(dbl(test$values), truth$values, tol=tol))
 stopifnot(identical(test$vectors, truth$vectors)) # both NULL
 
-test = eigen(cp_s, TRUE, FALSE)
-truth = eigen(cp_d, TRUE, FALSE)
+test = eigen(cp_s, symmetric=TRUE, only.values=FALSE)
+truth = eigen(cp_d, symmetric=TRUE, only.values=FALSE)
 stopifnot(all.equal(dbl(test$values), truth$values, tol=tol))
 stopifnot(all.equal(abs(dbl(test$vectors)), abs(truth$vectors), tol=tol))
