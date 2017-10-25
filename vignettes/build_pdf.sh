9c12121850ea6cb6252a604f2abeea4bba798df1
wrathematics <wrathematics@gmail.com>
2017-10-25 13:40:29 -0400

    add vignette

---
+++ vignettes/build_pdf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+fixVersion(){
+  PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
+  sed -i -e "s/myversion{.*}/myversion{${PKGVER}}/" $1
+}
+
+cleanVignette(){
+  rm -f *.aux *.bbl *.blg *.log *.out *.toc *.dvi
+}
+
+buildVignette(){
+  fixVersion $1
+  pdflatex $1
+  bibname=`echo "$1" | sed -e 's/\..*//'`
+  bibtex $bibname
+  pdflatex $1
+  pdflatex $1
+  Rscript -e "tools::compactPDF('$1', gs_quality='ebook')"
+}
+
+
+cleanVignette
+buildVignette float.Rnw
+cleanVignette
+
+
+mv -f *.pdf ../inst/doc/
+cp -f *.Rnw ../inst/doc/
