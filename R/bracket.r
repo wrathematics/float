06369fd8bc4204abe8388634ee9348773a81aa92
wrathematics <wrathematics@gmail.com>
2017-11-29 11:05:49 -0500

    add replacement method

--- R/bracket.r
+++ R/bracket.r
@@ -4,13 +4,15 @@
 #' 
 #' @param x
 #' A float vector/matrix.
-#' @param i,j
+#' @param i,j,...
 #' The indices.  Most combinations of integer/double/logical values will be
 #' treated the same as R does. One major difference is that \code{NA} values
 #' will not be tolerated.
 #' @param drop
 #' Logical. If TRUE, single column matrices will be treated as one-dimensional
 #' vectors.
+#' @param value
+#' The replacement value.
 #' 
 #' @return
 #' A float vector/matrix.
@@ -42,8 +44,40 @@ bracket_float32 = function(x, i, j, drop=TRUE)
   float32(d)
 }
 
+bracket_replace_float32 = function(x, i, j, ..., value)
+{
+  if (is.double(value))
+  {
+    dbl(x)[i, j] = value
+    return(x)
+  }
+  else if (is.integer(value))
+    value = fl(value)
+  
+  
+  if (missing(i) && missing(j))
+    x@Data[, ] = DATA(value)
+  else if (missing(i))
+    x@Data[, j] = DATA(value)
+  else if (missing(j))
+  {
+    if (isavec(x))
+      x@Data[i] = DATA(value)
+    else
+      x@Data[i, ] = DATA(value)
+  }
+  else
+    x@Data[i, j] = DATA(value)
+  
+  x
+}
+
 
 
 #' @rdname bracket
 #' @export
 setMethod("[", signature(x="float32"), bracket_float32)
+
+#' @rdname bracket
+#' @export
+setReplaceMethod("[", signature(x="float32", value="ANY"), bracket_replace_float32)
