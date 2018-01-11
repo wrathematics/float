9286526347cafdccc36e51308e07ab9278398fae
wrathematics <wrathematics@gmail.com>
2018-01-11 09:15:25 -0500

    fix str printing for matrices

--- R/str.r
+++ R/str.r
@@ -3,10 +3,17 @@ str.float32 = function(object, digits.d=strO$digits.d, ...)
 {
   strO = utils::strOptions()
   MAXLEN = 10L
+  len = length(object)
+  printlen = min(len, MAXLEN)
   
-  vals = dbl(object[1:(min(length(object), MAXLEN)), 1, drop=TRUE])
+  vals = dbl(object[1:printlen, 1, drop=TRUE])
   cat("Formal class 'float32' [package \"float\"] with 1 slot\n")
-  cat(paste0("..@ Data: int [1:", length(object), "] "))
+  
+  if (isavec(object))
+    cat(paste0("..@ Data: int [1:", len, "] "))
+  else
+    cat(paste0("..@ Data: int [1:", nrow(object), ", 1:", ncol(object), "] "))
+  
   cat(format(vals, digits=digits.d))
   if (length(vals) < length(object))
     cat(" ...")
