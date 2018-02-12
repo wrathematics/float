1b7684690a71ed9827562b17362061ad0e8bf59d
dselivanov <selivanov.dmitriy@gmail.com>
2018-02-12 22:33:51 +0400

    link full dir

--- R/02-flags.r
+++ R/02-flags.r
@@ -2,15 +2,12 @@ ldflags = function()
 {
   if (.Platform$OS.type != "windows")
   {
-    so.path = tools::file_path_as_absolute(system.file("libs", package = "float"))
-    flags = file.path(so.path, "float.so")
+    flags = tools::file_path_as_absolute(system.file("libs", package = "float"))
   }
   else
   {
     path = file.path("libs", .Platform$r_arch)
-    dll.path = tools::file_path_as_absolute(system.file(path, package = "float"))
-    flags = file.path(dll.path, "float.dll")
+    flags = tools::file_path_as_absolute(system.file(path, package = "float"))
   }
-  
-  cat("-L", flags, sep = "")
+  cat("-L", flags,  sep = "")
 }
