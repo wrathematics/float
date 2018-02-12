ee6710e45839c00f825fb1933b67c573c4c059c7
dselivanov <selivanov.dmitriy@gmail.com>
2018-02-12 12:14:15 +0400

    add "-L" to the float library path. Also make construction of the file paths portable.

--- R/02-flags.r
+++ R/02-flags.r
@@ -2,15 +2,15 @@ ldflags = function()
 {
   if (.Platform$OS.type != "windows")
   {
-    so.path = tools::file_path_as_absolute(system.file("libs", package="float"))
-    flags = paste0(so.path, "/float.so")
+    so.path = tools::file_path_as_absolute(system.file("libs", package = "float"))
+    flags = file.path(so.path, "float.so")
   }
   else
   {
-    path = paste0("libs/", .Platform$r_arch)
-    dll.path = tools::file_path_as_absolute(system.file(path, package="float"))
-    flags = paste0(dll.path, "/float.dll")
+    path = file.path("libs", .Platform$r_arch)
+    dll.path = tools::file_path_as_absolute(system.file(path, package = "float"))
+    flags = file.path(dll.path, "float.dll")
   }
   
-  cat(flags)
+  cat("-L", flags, sep = "")
 }
