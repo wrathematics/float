9376d228502e0156043e7f778239b86f6ed8f740
Charles Determan <cdetermanjr@gmail.com>
2018-06-05 11:32:54 -0500

    update to ldflags for windows path generation that is taken from those in Rcpp packages.  This will avoid the 'spaces' in Windows paths and normalize the paths as well.

--- R/02-flags.r
+++ R/02-flags.r
@@ -7,8 +7,15 @@ ldflags = function()
   }
   else
   {
+    # path = paste0("libs/", .Platform$r_arch)
+    # dll.path = tools::file_path_as_absolute(system.file(path, package="float"))
+    # flags = paste0(dll.path, "/float.dll")
+    
     path = paste0("libs/", .Platform$r_arch)
     dll.path = tools::file_path_as_absolute(system.file(path, package="float"))
+    dll.path <- normalizePath(dll.path)
+    dll.path <- utils::shortPathName(dll.path)
+    dll.path <- gsub("\\\\", "/", dll.path)
     flags = paste0(dll.path, "/float.dll")
   }
   
