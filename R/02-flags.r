e93856e2fa61b85772760e4cc21dc056bd4782ca
dselivanov <selivanov.dmitriy@gmail.com>
2019-01-24 10:18:44 +0400

    fixes #29

--- R/02-flags.r
+++ R/02-flags.r
@@ -6,7 +6,7 @@ get_os <- function()
 {
   os = tolower(Sys.info()["sysname"])
   names(os) = NULL
-  
+
   os
 }
 
@@ -15,18 +15,18 @@ get_os <- function()
 ldflags_string = function(static=FALSE)
 {
   install_path = "libs"
-  
+
   os = get_os()
   dynamic_link = (os == "linux" || os == "freebsd" || os == "darwin")  &&  !isTRUE(static)
-  
+
   if (nchar(.Platform$r_arch) > 0)
     path = file.path(install_path, .Platform$r_arch)
   else
     path = install_path
-  
+
   float_libs_dir_rel = system.file(path, package="float")
   float_libs_dir = tools::file_path_as_absolute(float_libs_dir_rel)
-  
+
   if (dynamic_link)
   {
     if (os == "darwin")
@@ -35,7 +35,7 @@ ldflags_string = function(static=FALSE)
       libfile = NULL
       for (libfile_test in libfiles)
       {
-        if (!file.exists(paste0(float_libs_dir, "/", libfile_test)))
+        if (file.exists(paste0(float_libs_dir, "/", libfile_test)))
         {
           libfile = libfile_test
           break
@@ -43,7 +43,7 @@ ldflags_string = function(static=FALSE)
       }
       if (is.null(libfile))
         stop(paste("unable to dynamically link: can't find any of:", paste(libfiles, collapse=", ")))
-      
+
       flags = paste0("-L", float_libs_dir, " ", float_libs_dir, "/", libfile, " -Wl,-rpath ", float_libs_dir)
     }
     else
@@ -51,14 +51,14 @@ ldflags_string = function(static=FALSE)
   }
   else
     flags = paste0(float_libs_dir, "/libfloat.a")
-  
+
   flags
 }
 
 ldflags = function(static=FALSE)
 {
   flags = ldflags_string(static=static)
-  
+
   cat(flags)
   invisible()
 }
@@ -68,15 +68,15 @@ ldflags = function(static=FALSE)
 cppflags_string = function()
 {
   install_path = "include"
-  
+
   if (nchar(.Platform$r_arch) > 0)
     path = file.path(install_path, .Platform$r_arch)
   else
     path = install_path
-  
+
   float_include_dir_rel = system.file(path, package="float")
   float_include_dir = tools::file_path_as_absolute(float_include_dir_rel)
-  
+
   flags = paste0("-I", float_include_dir)
   flags
 }
@@ -84,7 +84,7 @@ cppflags_string = function()
 cppflags = function()
 {
   flags = cppflags_string()
-  
+
   cat(flags)
   invisible()
 }
