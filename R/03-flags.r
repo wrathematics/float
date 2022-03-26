2574789f27794dfc8045dbac3122e7911abf00ac
wrathematics <wrathematics@gmail.com>
2022-03-26 14:36:23 -0400

    try harder checking for system sp linalg symbols

--- R/03-flags.r
+++ R/03-flags.r
@@ -1,13 +1,31 @@
 # linux: "linux"
 # mac: "darwin"
-# windows:
+# windows: "windows"
 # freebsd: "freebsd"
 get_os <- function()
 {
   os = tolower(Sys.info()["sysname"])
   names(os) = NULL
-
+  
   os
+}
+
+
+
+blas_ldflags_string = function()
+{
+  if (get_os() == "windows")
+    ""
+  else
+    blas_ldflags_nix() # defined by configure in R/02-libflags.r
+}
+
+lapack_ldflags_string = function()
+{
+  if (get_os() == "windows")
+    ""
+  else
+    lapack_ldflags_nix() # defined by configure in R/02-libflags.r
 }
 
 
@@ -15,18 +33,18 @@ get_os <- function()
 ldflags_string = function(static=FALSE)
 {
   install_path = "libs"
-
+  
   os = get_os()
   dynamic_link = (os == "linux" || os == "freebsd" || os == "darwin")  &&  !isTRUE(static)
-
+  
   if (nchar(.Platform$r_arch) > 0)
     path = file.path(install_path, .Platform$r_arch)
   else
     path = install_path
-
+    
   float_libs_dir_rel = system.file(path, package="float")
   float_libs_dir = tools::file_path_as_absolute(float_libs_dir_rel)
-
+  
   if (dynamic_link)
   {
     if (os == "darwin")
@@ -43,7 +61,7 @@ ldflags_string = function(static=FALSE)
       }
       if (is.null(libfile))
         stop(paste("unable to dynamically link: can't find any of:", paste(libfiles, collapse=", ")))
-
+      
       flags = paste0("-L", float_libs_dir, " ", float_libs_dir, "/", libfile, " -Wl,-rpath ", float_libs_dir)
     }
     else
@@ -51,14 +69,16 @@ ldflags_string = function(static=FALSE)
   }
   else
     flags = paste0(float_libs_dir, "/libfloat.a")
-
+  
   flags
 }
 
 ldflags = function(static=FALSE)
 {
-  flags = ldflags_string(static=static)
-
+  linalg_flags = paste(lapack_ldflags_string(), blas_ldflags_string())
+  float_flags = ldflags_string(static=static)
+  flags = paste(float_flags, linalg_flags)
+  
   cat(flags)
   invisible()
 }
@@ -68,15 +88,15 @@ ldflags = function(static=FALSE)
 cppflags_string = function()
 {
   install_path = "include"
-
+  
   if (nchar(.Platform$r_arch) > 0)
     path = file.path(install_path, .Platform$r_arch)
   else
     path = install_path
-
+  
   float_include_dir_rel = system.file(path, package="float")
   float_include_dir = tools::file_path_as_absolute(float_include_dir_rel)
-
+  
   flags = paste0("-I", float_include_dir)
   flags
 }
@@ -84,7 +104,7 @@ cppflags_string = function()
 cppflags = function()
 {
   flags = cppflags_string()
-
+  
   cat(flags)
   invisible()
 }
