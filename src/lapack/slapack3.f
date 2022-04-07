65bc8ab046d11693d889c27319b00e22745a0e1a
wrathematics <wrathematics@gmail.com>
2022-04-07 09:04:30 -0400

    fix gcc12 issue in internal lapack

--- src/lapack/slapack3.f
+++ src/lapack/slapack3.f
@@ -4885,7 +4885,7 @@
 *
 *        Compute Householder transform when M=1
 *
-         CALL SLARFG( N, A, A( 1, MIN( 2, N ) ), LDA, T )
+         CALL SLARFG( N, A, A( 1, MIN( 2, N ) ), LDA, T(1, 1) )
 *
       ELSE
 *
@@ -47561,5 +47561,3 @@
 *     End of STRTTP
 *
       END
-
-
